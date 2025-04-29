a05abf334f365d79bb90bb45548d8eb589b60386
Roger Oliva <rooliva@microsoft.com>
2025-04-29 00:24:46 -0600

    .

--- DeployScripts/Full.sh
+++ DeployScripts/Full.sh
@@ -6,8 +6,8 @@ export CLUSTER="arobuild"
 export SP_CLIENT_ID=""
 export SP_CLIENT_SECRET=""
 export VISIBILITY="Public"
-az group create --name $RESOURCEGROUP--location $LOCATION
-az network vnet create --resource-group $RESOURCEGROUP--name aro-vnet --address-prefixes 192.168.192.0/23
+az group create --name $RESOURCEGROUP --location $LOCATION
+az network vnet create --resource-group $RESOURCEGROUP --name aro-vnet --address-prefixes 192.168.192.0/23
 az network vnet subnet create --resource-group $RESOURCEGROUP--vnet-name aro-vnet --name master-subnet --address-prefixes 192.168.192.0/25 --service-endpoints Microsoft.ContainerRegistry
 az network vnet subnet create --resource-group $RESOURCEGROUP--vnet-name aro-vnet --name worker-subnet --address-prefixes 192.168.192.128/25 --service-endpoints Microsoft.ContainerRegistry
 az network vnet subnet update --name master-subnet --resource-group $RESOURCEGROUP--vnet-name aro-vnet --private-link-service-network-policies Disabled
