4d96fe61fac52d6b2bb1da75d1b6a736c1bae64c
Roger Oliva <rooliva@microsoft.com>
2025-04-29 00:19:31 -0600

    .

--- DeployScripts/Full.sh
+++ DeployScripts/Full.sh
@@ -1,21 +1,21 @@
 #/bin/bash
 az login --use-device-code --tenant 16b3c013-d300-468d-ac64-7eda0820b6d3
 export LOCATION="westus3"
-export RESOURCEGROUP="arogbbwestus3"
-export CLUSTER="aroclusterwestus3"
+export RESOURCEGROUP="arobuildw3"
+export CLUSTER="arobuild"
 export SP_CLIENT_ID=""
 export SP_CLIENT_SECRET=""
 export VISIBILITY="Public"
-az group create --name arogbbwestus3 --location westus3
-az network vnet create --resource-group arogbbwestus3 --name aro-vnet --address-prefixes 192.168.192.0/23
-az network vnet subnet create --resource-group arogbbwestus3 --vnet-name aro-vnet --name master-subnet --address-prefixes 192.168.192.0/25 --service-endpoints Microsoft.ContainerRegistry
-az network vnet subnet create --resource-group arogbbwestus3 --vnet-name aro-vnet --name worker-subnet --address-prefixes 192.168.192.128/25 --service-endpoints Microsoft.ContainerRegistry
-az network vnet subnet update --name master-subnet --resource-group arogbbwestus3 --vnet-name aro-vnet --private-link-service-network-policies Disabled
+az group create --name $RESOURCEGROUP--location $LOCATION
+az network vnet create --resource-group $RESOURCEGROUP--name aro-vnet --address-prefixes 192.168.192.0/23
+az network vnet subnet create --resource-group $RESOURCEGROUP--vnet-name aro-vnet --name master-subnet --address-prefixes 192.168.192.0/25 --service-endpoints Microsoft.ContainerRegistry
+az network vnet subnet create --resource-group $RESOURCEGROUP--vnet-name aro-vnet --name worker-subnet --address-prefixes 192.168.192.128/25 --service-endpoints Microsoft.ContainerRegistry
+az network vnet subnet update --name master-subnet --resource-group $RESOURCEGROUP--vnet-name aro-vnet --private-link-service-network-policies Disabled
 
-#az ad sp create-for-rbac -n arobuildgbbc --role contriburor --scopes /subscriptions/55318ed6-5d8a-4bd2-889f-10e502960c28/resourceGroups/arogbbwestus3 --skip-assignment
+#az ad sp create-for-rbac -n arobuildgbbc --role contriburor --scopes /subscriptions/55318ed6-5d8a-4bd2-889f-10e502960c28/resourceGroups/$RESOURCEGROUP--skip-assignment
 #az ad sp list --show-mine -o table
-#az aro create --resource-group arogbbwestus3 --name arolatamgbb --vnet aro-vnet --master-subnet master-subnet --worker-subnet worker-subnet --apiserver-visibility Private --ingress-visibility Private --domain arolatamgbb.jaropro.net --pull-secret @pull-secret.txt
-az aro create --resource-group arogbbwestus3 --name arolatamgbb --vnet aro-vnet --master-subnet master-subnet --worker-subnet worker-subnet --apiserver-visibility $VISIBILITY --ingress-visibility $VISIBILITY --domain jaropro.net --pull-secret @pull-secret.txt --client-id $SP_CLIENT_ID --client-secret $SP_CLIENT_SECRET
+az aro create --resource-group $RESOURCEGROUP --name $CLUSTER --vnet aro-vnet --master-subnet master-subnet --worker-subnet worker-subnet --apiserver-visibility $VISIBILITY --ingress-visibility $VISIBILITY --domain jaropro.net --pull-secret @pull-secret.txt --client-id $SP_CLIENT_ID --client-secret $SP_CLIENT_SECRET
+#az aro create --resource-group $RESOURCEGROUP --name $CLUSTER --vnet aro-vnet --master-subnet master-subnet --worker-subnet worker-subnet --apiserver-visibility $VISIBILITY --ingress-visibility $VISIBILITY --domain jaropro.net --pull-secret @pull-secret.txt
 
 #SECCION TO DELETE ALL or FULL DEPLOYMENT
 #az aro delete --name $CLUSTER --resource-group $RESOURCEGROUP --yes --no-wait
